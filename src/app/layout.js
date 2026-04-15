9b1fe9fa8dc030b3e60ea1a24a4f2fa18fb00528
yangnu <bin450579@gmail.com>
2026-04-15 23:54:44 +0900

    [Feat] metadata 추가 및 tailwind css 적용

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,10 @@
+import './globals.css';
+
+export const metadata = {
+  title: '학생 관리 시스템',
+  description: '학생 성적 및 상담 관리 시스템',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
